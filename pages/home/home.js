b664e822dee8200fc29af09235808bcab929af6b
胡双 <hd1611756908@163.com>
2019-01-11 15:38:04 +0800

    调整启事列表

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,18 +11,18 @@ Page({
       xw:[
         {
           id:1001,
-          imgsrc:'/pages/imgs/item.jpg',
+          primaryImg:'/pages/imgs/item.jpg',
           title:'学府路理工大学图书馆丢失华为手机一部',
           category:'手机',
-          time:'2019-01-12',
-          address:'哈尔滨市-南岗区',
-          detail:'本人在2019年1月12日晚在哈尔滨理工大学图书馆自习之后，将一部华为手机落在了图书馆的自习室桌面上，若有拾到的同学请联系我 QQ1611756998或者微信 18746092678'
+          loseDate:'2019-01-12',
+          region:'哈尔滨市-南岗区',
+          detailDesc:'本人在2019年1月12日晚在哈尔滨理工大学图书馆自习之后，将一部华为手机落在了图书馆的自习室桌面上，若有拾到的同学请联系我 QQ1611756998或者微信 18746092678'
         }
       ],
       xr:[
         {
           id: 1002,
-          imgsrc: '/pages/imgs/ren.png',
+          primaryImg: '/pages/imgs/ren.png',
           title: '学府路黑龙江大学学府夜市走失一个美丽漂亮的小萝莉',
           category: '女人',
           time: '2019-01-12',
@@ -33,7 +33,7 @@ Page({
       xc:[
         {
           id: 1003,
-          imgsrc: '/pages/imgs/gou.png',
+          primaryImg: '/pages/imgs/gou.png',
           title: '本人的拉布拉多狗狗在博物馆逛街时走失',
           category: '宠物',
           time: '2019-01-12',
@@ -44,12 +44,12 @@ Page({
       sz:[
         {
           id: 1004,
-          imgsrc: '/pages/imgs/bao.png',
+          primaryImg: '/pages/imgs/bao.png',
           title: '本人在2019年1月1日在哈尔滨工业大学图书馆捡到钱包一个',
           category: '包',
-          time: '2019-01-12',
-          address: '哈尔滨市-南岗区',
-          detail: '本人在2019年1月1日下午在哈尔滨工业大学图书馆自习室捡到黑色女士皮包一个,里面有现金若干,身份证,银行卡若干,如果失主看见此条消息可以联系我活着图书管管理员, QQ1611756998或者微信 18746092678'
+          loseDate: '2019-01-12',
+          region: '哈尔滨市-南岗区',
+          detailDesc: '本人在2019年1月1日下午在哈尔滨工业大学图书馆自习室捡到黑色女士皮包一个,里面有现金若干,身份证,银行卡若干,如果失主看见此条消息可以联系我活着图书管管理员, QQ1611756998或者微信 18746092678'
         }
       ]
     }
@@ -81,7 +81,18 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    var r = {};
+    wx.request({
+      url: 'http://localhost:8080/queryQsInfoList',
+      method: 'GET',
+      dataType: 'json',
+      success: function (res) {
+        that.setData({
+          result: res.data
+        });
+      }
+    })
   },
 
   /**
@@ -142,5 +153,24 @@ Page({
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + e.currentTarget.dataset.id,
     })
+  },
+  /**
+   * 下拉刷新
+   */
+  onPullDownRefresh() {
+    console.log("====");
+    var that = this;
+    var r = {};
+    wx.request({
+      url: 'http://localhost:8080/queryQsInfoList',
+      method: 'GET',
+      dataType: 'json',
+      success: function (res) {
+        that.setData({
+          result: res.data
+        });
+      }
+    })
+    wx.stopPullDownRefresh()
   }
 })
