5293d6f7760691aaa4d9939e940d200361e87dae
胡双 <hd1611756908@163.com>
2019-01-12 21:06:55 +0800

    新增授权页面和重新授权页面

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 //app.js
 App({
   onLaunch: function () {
-    //获取用户数据
-    
+    var that = this;
+    //判断是否授权,如果授权获取用户信息置位
+    wx.getSetting({
+      success(res){
+        if (res.authSetting['scope.userInfo']){
+          wx.getUserInfo({
+            success(e){
+              //console.log(e);
+              that.globalData.userInfo=e
+            }
+          })
+        }
+      }
+    });
   },
   globalData: {
-  
+    userInfo:null
   }
 })
